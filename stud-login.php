c8e31874413705e51959123a23d444572c528c2a
sammihhh <snagagas112903@gmail.com>
2025-09-09 18:38:29 +0800

    latest
    Co-Authored-By: PolarDredd <130895488+Josepolar@users.noreply.github.com>

---
+++ stud-login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LARS</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="stud-login.css">
+</head>
+<body>
+<div class="content">
+    <a href="teacher-login.php">
+        <img src="assets/signinbtn.png" alt="Sign In" class="signin-img">
+    </a>
+    <img src="assets/rightarr.png" alt="Right Arrow" class="rightarr-img">
+    <img src="assets/leftarr.png" alt="Left Arrow" class="leftarr-img">
+</div>
+
+
+</body>
+</html>
